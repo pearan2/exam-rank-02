c20e547391a7b9014fbfc715dda9cdaf430c1773
Hongkwon Lee <honlee@c8r6s14.42seoul.kr>
2021-04-22 14:45:20 +0900

    in gnl, without malloc return null guard, but pass exam

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: honlee <honlee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: honlee <honlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 12:39:34 by honlee            #+#    #+#             */
-/*   Updated: 2021/02/26 13:55:59 by honlee           ###   ########.fr       */
+/*   Updated: 2021/04/22 14:43:07 by honlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 
 int		ft_strlen(const char *str)
 {
-	int		ret;
-
-	ret = 0;
+	int ret = 0;
 	while (str[ret] != 0)
 		ret++;
 	return (ret);
@@ -25,17 +23,12 @@ int		ft_strlen(const char *str)
 
 char	*ft_charappend(char *target, char value)
 {
-	int		idx;
+	int		idx = -1;
 	char	*ret;
 
-	idx = 0;
-	if (!(ret = malloc(ft_strlen(target) + 2)))
-		return (0);
-	while (target[idx] != 0)
-	{
+	ret = malloc(ft_strlen(target) + 2);
+	while (target[++idx] != 0)
 		ret[idx] = target[idx];
-		idx++;
-	}
 	ret[idx] = value;
 	idx++;
 	ret[idx] = 0;
@@ -48,19 +41,15 @@ int		get_next_line(char **line)
 	int		rb;
 	char	buff;
 
-	if (!(*line = malloc(1)))
-		return (-1);
+	*line = malloc(1);
 	(*line)[0] = 0;
 	while ((rb = read(0, &buff, 1)) > 0)
 	{
 		if (buff == '\n')
 			return (1);
 		*line = ft_charappend(*line, buff);
-		if (*line == 0)
-			return (-1);
 	}
 	if (rb == -1)
 		return (-1);
-	else
-		return (0);
+	return (0);
 }
